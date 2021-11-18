98eade3f71c7a0d16343adc4dd1eb8f26b123b1b
swarup-e <swarup@datakaveri.org>
2021-11-18 16:09:02 +0530

    updated config

--- scheduler_script.py
+++ scheduler_script.py
@@ -5,9 +5,9 @@ import time
 
 def run():
     while True:
-        os.environ['TOKEN_UPDATED'] = '12345'
+        os.environ['TOKEN_UPDATED'] = st(12345)
         time.sleep(180)
-        os.environ['TOKEN_NEW'] = time.time()
+        os.environ['TOKEN_NEW'] = str(time.time())
 
 
 if __name__ == "__main__":
