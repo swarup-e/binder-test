b322102410881f3fed292396f8b7c7e25e702208
swarup-e <swarup@datakaveri.org>
2021-11-22 12:05:09 +0530

    Script fix

--- scheduler_script.py
+++ scheduler_script.py
@@ -15,7 +15,7 @@ headers = {
 response = requests.request("POST", url, headers=headers, data=payload)
 
 with open('data.json', 'w', encoding='utf-8') as f:
-    json.dump(json.loads(response.text))
+    json.dump(json.loads(response.json()))
 
 def run():
     while True:
@@ -24,7 +24,7 @@ def run():
             payload = f'client_id=binder-oauth-client-public&grant_type=refresh_token&refresh_token={json_data["refresh_token"]}'
         response = requests.request("POST", url, headers=headers, data=payload)
         with open('data.json', 'w', encoding='utf-8') as f:
-            json.dump(json.loads(response.text))
+            json.dump(json.loads(response.json()))
         time.sleep(120)
 
 if __name__ == "__main__":
