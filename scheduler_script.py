372682228aecfcec213e04abb9b0ac7f27760bc6
swarup-e <swarup@datakaveri.org>
2021-11-22 11:59:53 +0530

    Python write to file script

--- scheduler_script.py
+++ scheduler_script.py
@@ -1,14 +1,31 @@
 #!/usr/bin/env python3
 
 import os
+import requests
+import json
 import time
+
+url = "http://10.104.235.97:8080/auth/realms/test/protocol/openid-connect/token"
+
+payload=f'client_id=binder-oauth-client-public&grant_type=refresh_token&refresh_token={os.environ["KC_REFRESH_TOKEN"]}'
+headers = {
+    'Content-Type': 'application/x-www-form-urlencoded'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+with open('data.json', 'w', encoding='utf-8') as f:
+    json.dump(json.loads(response.text))
 
 def run():
     while True:
-        os.environ['TOKEN_UPDATED'] = str(12345)
-        time.sleep(180)
-        os.environ['TOKEN_NEW'] = str(time.time())
-
+        with open('data.json') as f:
+            json_data = json.load(f)
+            payload = f'client_id=binder-oauth-client-public&grant_type=refresh_token&refresh_token={json_data["refresh_token"]}'
+        response = requests.request("POST", url, headers=headers, data=payload)
+        with open('data.json', 'w', encoding='utf-8') as f:
+            json.dump(json.loads(response.text))
+        time.sleep(120)
 
 if __name__ == "__main__":
     run()
